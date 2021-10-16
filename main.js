ba3a2d13d7d2bbbf0f1e09ebcc8b845a42761011
Your Namef <obiobialo@aol.com>
2021-10-15 19:34:01 -0500

    GitHub Profile App

---
+++ main.js
@@ -0,0 +1,17 @@
+import * as v from "./js/variables.js";
+import { getUser, errorMessage } from "./js/functions.js";
+
+v.form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    // let user = v.search.value;
+    let user = v.search.value.split(" ").join("");
+
+    if (user === "") {
+        errorMessage("Input cannot be empty");
+        console.log("blank");
+    } else {
+        getUser(user);
+        v.search.value = "";
+    }
+    
+})
