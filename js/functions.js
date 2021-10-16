619ebf72692cec3391bb280d9c4a2974ae3e40d3
Your Namef <obiobialo@aol.com>
2021-10-15 19:58:01 -0500

    GitHub Profile App

--- js/functions.js
+++ js/functions.js
@@ -20,7 +20,11 @@ async function getRepos(username) {
     const data = await response.json();
 
     console.log(data);
+    displayRepos(data);
 }
+
+// ERROR MESSAGE FUNCTION
+
 
 
 
@@ -54,4 +58,26 @@ function displayData(user) {
     <p><i class="fas fa-marker-alt"></i>${user.location}</p> 
     `;
     v.profile.innerHTML = html;
+}
+
+
+
+// DISPLAY REPOS
+function displayRepos(repoData) {
+    let repo_data = repoData.map((repo) => {
+        return `
+        <span class="repo border border-rounded p3">
+                <a href="${repo.html_url}" target="_blank" rel="noopener">${repo.name}</a>
+                <p>
+                  <strong>Stars: ${repo.stargazers_count} | </strong>
+                  <strong>Watchers: ${repo.watchers_count} | </strong>
+                  <strong>Forks: ${repo.forks_count} | </strong>
+                </p>
+              </span>
+        `;
+    })
+    
+    // v.repos.innerHTML = repo_data;
+    v.repos.innerHTML = repo_data.slice(0, 8).join("");
+    document.querySelector(".hide").style.display = "block";
 }
